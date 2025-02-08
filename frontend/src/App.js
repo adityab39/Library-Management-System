7a93aac6f2272e41be3680420cca793c1d9a0d00
adityab39 <adityabahulikar39@gmail.com>
2025-02-08 15:08:27 -0500

    intial frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-purple-700">Hello, Tailwind is Working! 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
